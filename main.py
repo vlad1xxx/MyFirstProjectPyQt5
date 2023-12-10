f71fcaf2af4c8ef9edfed236ebb794506599e4fc
vlad1xxx <vlad1x2807@gmail.com>
2023-12-10 23:26:21 +0300

    update

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import json
 from io import StringIO
 
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, qApp
+
 
 # from dialogs import FirstMiss, InfoPEP8, Congratulation
 
@@ -26,6 +28,7 @@ class PythonExecutor:
 
 class MyWidget(QMainWindow):
     def __init__(self):
+        self.name_object = None
         self.first_miss = False
         self.count_heart = 5
         self.mistakes = 0
@@ -38,11 +41,18 @@ class MyWidget(QMainWindow):
 
     def initUI(self):
         self.update_hp()
-        self.createvar_btn.clicked.connect(self.choose_theme)
-        # self.continue_createvar.clicked.connect(self.next_page_createvar)
-        # self.continue_createvar_2.clicked.connect(self.next_page_createvar)
-        # self.check_createvar_1.clicked.connect(self.check_answer_createvar_1)
-        # self.res_createvar_1.textChanged.connect(self.change_size)
+        self.update_data()
+        self.createvar_btn.clicked.connect(self.choose_themes)
+        self.continue_createvar_btn_1.clicked.connect(self.next_page_createvar)
+        self.continue_createvar_btn_2.clicked.connect(self.next_page_createvar)
+        self.continue_createvar_btn_3.clicked.connect(self.next_page_createvar)
+        self.continue_createvar_btn_4.clicked.connect(self.check_answer_createvar_1)
+        self.answer_edit_1.textChanged.connect(self.change_size)
+        self.frame_console_1.hide()
+        self.continue_createvar_btn_5.clicked.connect(self.check_answer_createvar_2)
+        self.frame_console_2.hide()
+        self.continue_createvar_btn_6.clicked.connect(self.check_answer_createvar_3)
+
         # self.res_createvar_2.textChanged.connect(self.change_size)
         # self.res_createvar_3.textChanged.connect(self.change_size)
         # self.res_createvar_4.textChanged.connect(self.change_size)
@@ -65,19 +75,20 @@ class MyWidget(QMainWindow):
         self.save_btn.clicked.connect(self.save_file)
         self.exit_btn.clicked.connect(self.exit_btn_pressed)
 
-    def choose_theme(self):
+    def choose_themes(self):
         text = self.sender().text()
         if self.check_hp():
             if text == 'Создание переменных':
-                self.main_windows.setCurrentIndex(1)
-                self.page_task.setCurrentIndex(0)
+                self.main_menu.setCurrentIndex(1)
+                self.themes.setCurrentIndex(0)
+                self.tasks_createvar.setCurrentIndex(0)
                 self.cur_num_main_window = 0
         else:
             pass
             # TODO: добавить окно которое говорит о том что надо подождать
 
     def next_page_createvar(self):
-        self.page_task.setCurrentIndex(self.page_task.currentIndex() + 1)
+        self.tasks_createvar.setCurrentIndex(self.tasks_createvar.currentIndex() + 1)
         # self.progress_tasks.setValue(self.progress_createvar_1.value() + 12)
 
     def main_window(self):
@@ -90,83 +101,85 @@ class MyWidget(QMainWindow):
         res.setFixedWidth(new_width)
 
     def check_answer_createvar_1(self):
-        if self.res_createvar_1.text() == 'city':
-            self.check_createvar_1.setText('Продолжить')
-            self.check_createvar_1.clicked.connect(self.next_page_createvar)
+        if self.answer_edit_1.text() == 'car = "Porsche"' or self.answer_edit_1.text() == "car = 'Porsche'":
+            self.continue_createvar_btn_4.setText('Продолжить')
+            self.continue_createvar_btn_4.clicked.connect(self.next_page_createvar)
+            self.name_object = self.progress_createvar_1
+            self.start_progress_bar()
         else:
             self.check_first_miss()
             self.count_heart -= 1
             self.mistakes += 1
             self.update_data()
+            if self.count_heart == 0:
+                self.end_createvar(False)
+            self.update_hp_timer()
 
     def check_answer_createvar_2(self):
-        if self.res_createvar_2.text() == '"Porsche"':
-            self.check_createvar_2.setText('Продолжить')
-            self.check_createvar_2.clicked.connect(self.next_page_createvar)
-        else:
-            self.check_first_miss()
-            self.count_heart -= 1
-            self.mistakes += 1
-            self.update_data()
+        self.continue_createvar_btn_5.setText('Продолжить')
+        self.continue_createvar_btn_5.clicked.connect(self.next_page_createvar)
+        self.frame_console_1.show()
+        self.name_object = self.progress_createvar_2
+        self.start_progress_bar()
 
     def check_answer_createvar_3(self):
-        if self.res_createvar_3.text() == '=':
-            self.check_createvar_3.setText('Продолжить')
-            self.check_createvar_3.clicked.connect(self.next_page_createvar)
+        if self.answer_edit_4.toPlainText() == 'name = "Hello, Qt!"\nprint(name)' \
+                or self.answer_edit_4.toPlainText() == "name = 'Hello, Qt!'\nprint(name)":
+            self.continue_createvar_btn_6.setText('Завершить')
+            self.continue_createvar_btn_6.clicked.connect(self.open_num_btn)
+            self.frame_console_2.show()
+            self.progress_createvar_3.setValue(100)
         else:
             self.check_first_miss()
             self.count_heart -= 1
             self.mistakes += 1
             self.update_data()
+            if self.count_heart == 0:
+                self.end_createvar()
+            self.update_hp_timer()
 
-    def check_answer_createvar_4(self):
-        if (self.res_createvar_4.text() == 'step_1 = "step"' and self.res_createvar_5.text() == 'step_2 = "by"'
-                and self.res_createvar_6.text() == 'step_3 = "step"'):
-            self.check_createvar_4.setText('Продолжить')
-            self.check_createvar_4.clicked.connect(self.next_page_createvar)
-        else:
-            self.check_first_miss()
-            self.count_heart -= 1
-            self.mistakes += 1
-            self.update_data()
+    def end_createvar(self):
+        self.main_menu.setCurrentIndex(0)
+        self.tasks_createvar.setCurrentIndex(0)
+        self.continue_createvar_btn_6.disconnect()
+        self.answer_edit_1.clear()
+        self.answer_edit_1.resize(21, 31)
+        self.answer_edit_4.clear()
+        self.progress_createvar_1.setValue(0)
+        self.progress_createvar_2.setValue(0)
+        self.progress_createvar_3.setValue(0)
+        self.frame_console_1.hide()
+        self.frame_console_2.hide()
+        self.continue_createvar_btn_4.setText('Проверить')
+        self.continue_createvar_btn_4.disconnect()
+        self.continue_createvar_btn_5.setText('Проверить')
+        self.continue_createvar_btn_5.disconnect()
+        self.continue_createvar_btn_6.setText('Проверить')
+        self.continue_createvar_btn_4.clicked.connect(self.check_answer_createvar_1)
+        self.continue_createvar_btn_5.clicked.connect(self.check_answer_createvar_2)
+        self.continue_createvar_btn_6.clicked.connect(self.check_answer_createvar_3)
 
-    def check_answer_createvar_5(self):
-        self.check_createvar_5.setText('Продолжить')
-        self.check_createvar_5.clicked.connect(self.next_page_createvar)
-        self.frame_19.setHidden(False)
-        self.correct_answer()
+    def open_num_btn(self):
+        self.progress_createvar_3.setValue(0)
+        self.num_btn.setEnabled(True)
+        self.num_btn.setStyleSheet('''color: white;
+        background: rgb(64,66,115);
+        border-radius: 5px; font-weight:
+        bold; font-size: 16px;
+        ''')
+        self.progress_basics_part1.setValue(25)
+        self.end_createvar()
 
-    def check_answer_createvar_6(self):
-        if self.res_createvar_9.text() == 'print("GO!")':
-            self.check_createvar_6.setText('Продолжить')
-            self.check_createvar_6.clicked.connect(self.next_page_createvar)
-            self.frame_22.setHidden(False)
-        else:
-            self.check_first_miss()
-            self.count_heart -= 1
-            self.mistakes += 1
-            self.update_data()
-
-    def check_answer_createvar_7(self):
-        if self.res_createvar_11.text() == 'print(greeting)':
-            self.check_createvar_7.setText('Завершить')
-            self.check_createvar_7.clicked.connect(self.exit)
-            self.xp += 100
-        else:
-            self.check_first_miss()
-            self.count_heart -= 1
-            self.mistakes += 1
-            self.update_data()
-
-    # def check_first_miss(self):
-    #     if not self.first_miss:
-    #         self.first_miss = True
-    #         self.dialog = FirstMiss(self)
-    #         self.dialog.exec_()
-    #
-    # def get_info_pep8(self):
-    #     self.dialog = InfoPEP8(self)
-    #     self.dialog.exec_()
+    def check_first_miss(self):
+        #     if not self.first_miss:
+        #         self.first_miss = True
+        #         self.dialog = FirstMiss(self)
+        #         self.dialog.exec_()
+        #
+        # def get_info_pep8(self):
+        #     self.dialog = InfoPEP8(self)
+        #     self.dialog.exec_()
+        pass
 
     def run_code(self):
         code = self.text_edit.toPlainText()
@@ -193,12 +206,11 @@ class MyWidget(QMainWindow):
                 file.write(self.text_edit.toPlainText())
 
     def exit_btn_pressed(self):
-        qApp.setStyleSheet("QMessageBox QPushButton{color: white;}")
-        reply = QMessageBox.information(self, 'Подтверждение', '<p style="color: white;"> Вы уверены, что хотит '
-                                                               'выйти?</p>', QMessageBox.Yes | QMessageBox.No)
+        reply = QMessageBox.information(self, 'Подтверждение', 'Вы уверены, что хотите выйти?',
+                                        QMessageBox.Yes | QMessageBox.No)
 
         if reply == QMessageBox.Yes:
-            self.main_window()
+            self.end_createvar()
 
     # def exit(self):
     #     congratulation = Congratulation()
@@ -211,6 +223,7 @@ class MyWidget(QMainWindow):
     def update_data(self):
         self.hp_btn.setText(f'💖 {self.count_heart}')
         self.hp_text.setText(' 💖' * self.count_heart)
+        self.hp_text.adjustSize()
 
     def check_hp(self):
         return bool(self.count_heart)
@@ -248,9 +261,39 @@ class MyWidget(QMainWindow):
         with open('last_update_time.json', 'w') as file:
             json.dump(data, file, indent=2)
 
+    def start_progress_bar(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.increase_progress_bar)
+        self.timer.start(10)
+
+    def increase_progress_bar(self):
+        value = self.name_object.value()
+        if value <= 100:
+            self.name_object.setValue(value + 5)
+        else:
+            self.timer.stop()
+            self.timer.deleteLater()
+
+    def update_hp_timer(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_count_hp)
+        self.timer.start(600000)
+
+    def update_count_hp(self):
+        self.count_heart += 1
+        if self.count_heart != 5:
+            self.update_hp_timer()
+        else:
+            self.timer.deleteLater()
+        self.update_data()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
+
+# car = "Porsche"
+name = "Hello, Qt!"
+print(name)
